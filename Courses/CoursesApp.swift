5092149def0fc832ab8835d8a4fe15428e707de4
EsAlco <90135660+EsAlco@users.noreply.github.com>
2022-02-02 11:26:00 +0100

    Initial Commit

---
+++ Courses/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Courses
+//
+//  Created by Esther Alcoceba Gutiérrez de León on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
